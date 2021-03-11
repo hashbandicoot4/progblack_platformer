63ec287feefd73c21c74709473370365496e6c6d
Hashim Hussain <hashimh@hotmail.co.uk>
2021-03-11 19:02:44 +0000

    Add project files.

---
+++ FirstGame/Game1.cs
@@ -0,0 +1,136 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using System;
+
+namespace FirstGame
+{
+    //This class inherits from the Game class
+    public class Game1 : Game
+    {
+        // Declaring a new texture
+        Texture2D ballTexture;
+        // Declaring a variable for position
+        Vector2 ballPosition;
+        // Declaring a variable for speed
+        float ballSpeed;
+
+
+
+
+        // Variables used to draw on the screen
+        private GraphicsDeviceManager _graphics;
+        private SpriteBatch _spriteBatch;
+
+
+
+
+
+
+        // Used to initialise the starting variables
+        public Game1()
+        {
+            _graphics = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";
+            IsMouseVisible = true;
+        }
+
+
+
+
+
+
+        // Used to load any non-graphic related content
+        protected override void Initialize()
+        {
+            // Used to initialise the position and speed
+            ballPosition = new Vector2(_graphics.PreferredBackBufferWidth / 2,
+            _graphics.PreferredBackBufferHeight / 2);
+            ballSpeed = 400f;
+
+            base.Initialize();
+        }
+
+
+
+
+
+
+        // Loads the game content
+        protected override void LoadContent()
+        {
+            _spriteBatch = new SpriteBatch(GraphicsDevice);
+
+            // Load the ball
+            ballTexture = Content.Load<Texture2D>("pokeball");
+
+        }
+
+
+
+
+
+
+
+        // Called many times to update the game state
+        protected override void Update(GameTime gameTime)
+        {
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+                Exit();
+
+            // For the user input, which is stored
+            var kstate = Keyboard.GetState();
+            if (kstate.IsKeyDown(Keys.Up))
+                ballPosition.Y -= ballSpeed * (float)gameTime.ElapsedGameTime.TotalSeconds;
+            if (kstate.IsKeyDown(Keys.Down))
+                ballPosition.Y += ballSpeed * (float)gameTime.ElapsedGameTime.TotalSeconds;
+            if (kstate.IsKeyDown(Keys.Left))
+                ballPosition.X -= ballSpeed * (float)gameTime.ElapsedGameTime.TotalSeconds;
+            if (kstate.IsKeyDown(Keys.Right))
+                ballPosition.X += ballSpeed * (float)gameTime.ElapsedGameTime.TotalSeconds;
+
+            // To create the bounds for the ball
+            if (ballPosition.X > _graphics.PreferredBackBufferWidth - ballTexture.Width / 2)
+                ballPosition.X = _graphics.PreferredBackBufferWidth - ballTexture.Width / 2;
+            else if (ballPosition.X < ballTexture.Width / 2)
+                ballPosition.X = ballTexture.Width / 2;
+
+            if (ballPosition.Y > _graphics.PreferredBackBufferHeight - ballTexture.Height / 2)
+                ballPosition.Y = _graphics.PreferredBackBufferHeight - ballTexture.Height / 2;
+            else if (ballPosition.Y < ballTexture.Height / 2)
+                ballPosition.Y = ballTexture.Height / 2;
+
+            base.Update(gameTime);
+        }
+
+
+
+
+
+
+
+        // Called many times to update the screen
+        protected override void Draw(GameTime gameTime)
+        {
+            // Colour the background beige
+            GraphicsDevice.Clear(Color.Beige);
+
+            // Draw the ball onto the screen, repositioning it to the centre
+            _spriteBatch.Begin();
+            _spriteBatch.Draw(
+                ballTexture,
+                ballPosition,
+                null,
+                Color.White,
+                0f,
+                new Vector2(ballTexture.Width / 2, ballTexture.Height / 2),
+                Vector2.One,
+                SpriteEffects.None,
+                0f
+            );
+            _spriteBatch.End();
+
+            base.Draw(gameTime);
+        }
+    }
+}
---
+++ FirstGame/Models/Animation.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FirstGame.Models
+{
+    class Animation
+    {
+    }
+}
